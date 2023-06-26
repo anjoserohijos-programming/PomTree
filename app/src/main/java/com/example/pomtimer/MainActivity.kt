33ca184a3b76ad62bff2d4142ab1c7dfbc9d89b8
Mac12880 <marc.juan6.mj@gmail.com>
2023-06-26 09:49:33 +0800

    working changing img on percentage on completed task

--- app/src/main/java/com/example/pomtimer/MainActivity.kt
+++ app/src/main/java/com/example/pomtimer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -13,11 +14,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.*
 import java.util.UUID
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,10 +31,39 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+@Composable
+fun  MyImage(){
+    val img1 = "R.drawable.small_seed_level_0"
+    val img2 = "R.drawable.small_sapling_level_1_1"
+    val img3 = "R.drawable.small_sapling_level_2"
+    val img4 = "R.drawable.small_sapling_level_3"
+    val img5 = "R.drawable.small_sapling_level_4"
+    var checkedTodoListCount = 8
+    var maxTodoListCount = 10
+    var img2Per = kotlin.math.ceil(maxTodoListCount * 0.2)
+    var img3Per = kotlin.math.ceil(maxTodoListCount * 0.5)
+    var img4Per = kotlin.math.ceil(maxTodoListCount * 0.8)
+    if (checkedTodoListCount == maxTodoListCount){
+        Image(painter = painterResource(id = R.drawable.small_sapling_level_4), contentDescription = "Image")
+    }else if(checkedTodoListCount >= img4Per){
+        Image(painter = painterResource(id = R.drawable.small_sapling_level_3), contentDescription = "Image")
+    }
+    else if(checkedTodoListCount >= img3Per){
+        Image(painter = painterResource(id = R.drawable.small_sapling_level_2), contentDescription = "Image")
+    }
+    else if(checkedTodoListCount >= img2Per){
+        Image(painter = painterResource(id = R.drawable.small_sapling_level_1_1), contentDescription = "Image")
+    }
+    else{
+        Image(painter = painterResource(id = R.drawable.small_seed_level_0), contentDescription = "Image")
+    }
+
+}
+
 
 @Composable
 fun Main()  {
-    var minutes by remember { mutableStateOf(2) }
+    var minutes by remember { mutableStateOf(25) }
     var seconds by remember { mutableStateOf(0) }
     var isRunning by remember { mutableStateOf(false) }
 
@@ -82,14 +114,19 @@ fun Main()  {
                     Text("Long break")
                 }
             }
-            Canvas(
-                modifier = Modifier
-                    .size(200.dp)
-                    .clip(CircleShape)
-                    .padding(8.dp)
-            ) {
-
-                val totalSeconds = minutes * 60 + seconds
+//            Canvas(
+//                modifier = Modifier
+//                    .size(200.dp)
+//                    .clip(CircleShape)
+//                    .padding(8.dp)
+//            ) {
+//
+//                val totalSeconds = minutes * 60 + seconds
+//            }
+            Column(modifier = Modifier .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                MyImage()
             }
             Row(
                 modifier = Modifier.padding(top = 16.dp),
